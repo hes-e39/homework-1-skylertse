f990cd5705f04f95bb9a872fb4774e4425fc33b5
Skyler Tse <68565116+skylertse@users.noreply.github.com>
2024-09-20 13:41:57 -0400

    Update index.js
    HW1

--- index.js
+++ index.js
@@ -11,8 +11,40 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @returns Promise
  */
 const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+  return fetch(COLORS)
+    .then(response => response.json())
+    .then(colors => {
+      // 根据过滤条件筛选数据; 我好累啊 （im so tired) lol
+      let filteredColors = colors;
+      // console.log(filteredColors);
+      if (name) {
+        filteredColors = filteredColors.filter(color => 
+          color.name.toLowerCase().includes(name.toLowerCase())
+        );
+      }
+      if (hex) {
+        filteredColors = filteredColors.filter(color => 
+          color.hex.toLowerCase().includes(hex.toLowerCase())
+        );
+      }
+      if (compName) {
+        filteredColors = filteredColors.filter(color =>
+        color.comp.some(C=>C.name.toLowerCase().includes(compName.toLowerCase()))
+        );
+      }
+
+      if (compHex) {
+        filteredColors = filteredColors.filter(color =>
+        color.comp.some(C=>C.hex.toLowerCase().includes(compHex.toLowerCase()))
+        );
+      }
+
+
+      return filteredColors;
+    }
+    )
 };
+
 
 // Leave this here
 export default fetchColors;
